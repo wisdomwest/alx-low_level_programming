4f2a3f526a4d4cd59c2f468d6745f076ba32e853
wisdomwest <harmonykoome@gmail.com>
2023-03-21 09:17:01 -0700

    fixed errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,39 +10,37 @@ void print_times_table(int n)
 {
 	if (n < 0 || n > 15)
 	{
-		return ();
-	}
-	for (int i = 0; i <= n; i++)
-	{
-		_putchar('0');
+		for (int i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (int j = 0; j <= n; j++)
+			{
+				int prod = i * j;
 
-	for (int j = 0; j <= n; j++)
-	{
-		int prod = i * j;
-			if (prod < 10)
-			{
-				_putchar(prod + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (prod < 10)
+				{
+					_putchar(prod + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (prod < 100)
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			else if (prod < 100)
-			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((prod / 100) + '0');
-				_putchar(((prod / 10) % 10) + '0');
-				_putchar((prod % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-	}
-
-	_putchar('\n');
+			_putchar('\n');
+		}
 	}
 }
