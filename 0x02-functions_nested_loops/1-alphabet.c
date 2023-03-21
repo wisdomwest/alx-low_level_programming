270625774e011feffc02054fcbab8cc0ad2ed8c0
wisdomwest <harmonykoome@gmail.com>
2023-03-21 06:23:19 -0700

    alpahbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet -  print alphabet in small letters
+ */
+
+void print_alphabet(void)
+{
+	char x:
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+
+	_putchar('\n');
+}
