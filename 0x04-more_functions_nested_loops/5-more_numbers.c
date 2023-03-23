4c9ed08b653df179ab467afeb6a02e7e900e8462
wisdomwest <harmonykoome@gmail.com>
2023-03-23 06:42:13 -0700

    printing series of numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 - 14 ten times
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+			else
+				_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
