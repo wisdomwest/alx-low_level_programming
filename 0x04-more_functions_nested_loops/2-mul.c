7b7dd1f261f4b8a84416570038ab7591be6aa502
wisdomwest <harmonykoome@gmail.com>
2023-03-23 04:48:26 -0700

    fixing error

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,10 @@
  * mul - multiplies number
  * @a: first int
  * @b: second int
- * @c: result
  * Return: multiplication
  */
 
 int mul(int a, int b);
 {
-	int c;
-
-	c = a * b;
-	return (c);
+	return (a * b);
 }
