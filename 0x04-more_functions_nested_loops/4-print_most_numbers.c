2ddac88a5da10f8dbfeed6dbe596494307d0d6f6
wisdomwest <harmonykoome@gmail.com>
2023-03-23 05:42:11 -0700

    printing specific numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  * Return: 0
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		if (!(i == 2 && i == 4))
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
