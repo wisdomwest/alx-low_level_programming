868d562ae0a6e6a0a16719569555031923d63a4f
wisdomwest <harmonykoome@gmail.com>
2023-03-23 05:51:23 -0700

    fixing errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+	if (!(c == 2 || c == 4))
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
