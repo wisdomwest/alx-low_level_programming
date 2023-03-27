9b1b360afc9fe47a09f3ba70dbf07b828ac9bb37
wisdomwest <harmonykoome@gmail.com>
2023-03-27 06:49:46 -0700

    errors

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * re_string - reverse the string such that char 7 is now 2 and 2 now 7
+ * @s: string
+ * Return 0
+ */
+
+void rev_string(char *s)
+{
+	int i, h1, h2;
+	char j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+
+	h2 = i - 1;
+
+	for (h1 = 0; h1 < h2; h1++)
+	{
+		j = s[h1];
+		s[h1] = s[h2];
+		s[h2] = j;
+		h2--;
+	}
+	
+	for (b = 0; b < a / 2; b++)
+	{
+		j = s[h1];
+		s[h1] = s[i - h1 - 1];
+		s[i - h1 - 1] = j;
+	}
+}
