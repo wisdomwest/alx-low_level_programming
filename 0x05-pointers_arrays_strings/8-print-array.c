f34772bb510ff28b66089df3d826e8eaac74d285
wisdomwest <harmonykoome@gmail.com>
2023-03-27 07:42:02 -0700

    print n terms of an array

---
+++ 0x05-pointers_arrays_strings/8-print-array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements in the array
+ * @a: array to check
+ * @n: number n
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			prinf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
