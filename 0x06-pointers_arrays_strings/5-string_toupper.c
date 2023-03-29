1b9a7a8b464f070d5b84125152d578c3ea208133
wisdomwest <harmonykoome@gmail.com>
2023-03-29 02:12:12 -0700

    change to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowecase to uppercase
+ * @str: string
+ * Return: rev string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'Z')
+		{
+			str[i] = str[i] - 32;
+		}
+
+	}
+
+	return (str);
+}
