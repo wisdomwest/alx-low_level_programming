42e26f7c765b4892e809d3a446ee8e4ba8c9b4c2
wisdomwest <harmonykoome@gmail.com>
2023-03-30 01:31:32 -0700

    error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *s)
 	int count = 0, i;
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
+	if (*(s + count) >= 'a' && *(s + count) <= 'z')
 	{
 		*(s + count) = *(s + count) - 32;
 		count++;
@@ -23,7 +23,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == separators[i])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+				if ((*(s + (count + 1)) >= 'a') && (*(s + (count + 1)) <= 'z'))
 				{
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 					break;
