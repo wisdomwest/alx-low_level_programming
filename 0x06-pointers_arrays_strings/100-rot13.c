79ee8528e0842ed60ff869e5047873ebb4bd16a0
wisdomwest <harmonykoome@gmail.com>
2023-03-30 02:12:28 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = *(*s + 1) + 13;
+				break;
+			}
+		}
+	}
+	return (s);
+}
