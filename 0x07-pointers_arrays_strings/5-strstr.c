6e8c97eb9b93dc855321a4012618673068a33299
wisdomwest <harmonykoome@gmail.com>
2023-04-03 06:42:05 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - find string in string
+ * @haystack: larger string
+ * @needle: string to search in larger string
+ * Return: pointer to beging og the string found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h != '\0' && *n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+
+		haystack++
+	}
+}
