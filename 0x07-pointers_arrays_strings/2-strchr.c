51adb32ffcab5c0ad5870f899964b2713477972a
wisdomwest <harmonykoome@gmail.com>
2023-04-03 05:13:52 -0700

    strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - check occarance of char in string
+ * @s: string to check
+ * @c: char to be chacked in the string
+ * Return: pointer to occarance to the char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+
+	if ( c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
