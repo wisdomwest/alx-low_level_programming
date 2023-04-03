cd1e5b614b61f3ffc572d15df3f4b329eb6e2afe
wisdomwest <harmonykoome@gmail.com>
2023-04-03 05:42:54 -0700

    i dont know hats happening

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strspn - check how many times certain char are in string
+ * @s: string to be checked
+ * @accept: char to check if are in s
+ * Return: count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '\0' && _strchr(accept, *s) != 0)
+	{
+		count++;
+		s++
+	}
+}
