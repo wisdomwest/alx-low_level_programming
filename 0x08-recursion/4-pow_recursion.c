0043496bbda1b059f0134b9e8ecccc3251eeb334
wisdomwest <harmonykoome@gmail.com>
2023-04-05 04:32:37 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _pow_recursion - find power to sth of an int
+ *  @x: int 1
+ *  @y: power
+ *  Return: answer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
