84c1adb7e47c14007418bbdf92e8c51565cdb593
wisdomwest <harmonykoome@gmail.com>
2023-04-05 04:18:59 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - find factorial
+ * @n: interger to !
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
