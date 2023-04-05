3e015afbd21812e9676704a863f966b7a259672d
wisdomwest <harmonykoome@gmail.com>
2023-04-05 13:28:52 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * wildcmp - comapre two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 or 1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+
+		else
+		{
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		}
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
