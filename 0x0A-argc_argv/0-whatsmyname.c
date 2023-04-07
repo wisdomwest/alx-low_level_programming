2f4b6aaca5777e77e6ed010c63640a4fbd815017
wisdomwest <harmonykoome@gmail.com>
2023-04-07 11:24:07 -0700

    hope it works

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - entry point
@@ -10,6 +9,15 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	char *name = argv[0];
+
+	while (*name)
+	{
+		_putchar(*name);
+		name++;
+	}
+
+	_putchar('\n');
+
 	return (0);
 }
