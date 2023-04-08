7a15865b447485509fc635531dd8dd742295c2e4
wisdomwest <harmonykoome@gmail.com>
2023-04-08 01:32:19 -0700

    argument counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: arguments
+ * @argc: argument counter
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
