9cec5e5f76b1b695ea82e0603ca13642cb51b34a
wisdomwest <harmonykoome@gmail.com>
2023-04-13 06:03:01 -0700

    vi 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	}
 
-	if (new_size <=  old_size)
+	if (new_size ==  old_size)
 	{
 		return (ptr);
 	}
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr != NULL)
 	{
-		if (old_size < new_size)
+		if (new_size > old_size)
 		{
 			memcpy(new_ptr, ptr, old_size);
 		}
