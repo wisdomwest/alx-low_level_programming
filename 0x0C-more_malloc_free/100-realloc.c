7b9526a8b66ea4217e50982b79b54678e906a565
wisdomwest <harmonykoome@gmail.com>
2023-04-13 06:16:12 -0700

    work now please

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,6 +34,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	new_ptr = malloc(new_size);
 
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
 	if (new_ptr != NULL)
 	{
 		if (new_size > old_size)
