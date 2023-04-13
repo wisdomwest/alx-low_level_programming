40fd1fe7508639605cf93d07422fba9e47f9976d
wisdomwest <harmonykoome@gmail.com>
2023-04-13 02:37:38 -0700

    error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,10 +37,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (j = 0; j < n; j++)
 	{
-		cat[i = j] = s2[j];
+		cat[i + j] = s2[j];
 	}
 
-	cat[i = j] = '\0';
+	cat[i + j] = '\0';
 
 	return (cat);
 }
