b2223d6d84f2d2956a094d77ae98b00fe28fda56
wisdomwest <harmonykoome@gmail.com>
2023-04-13 05:23:34 -0700

    task 5

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - similar to realloc
+ * @ptr: old pointer
+ * @old_size: old size of memory
+ * @new_size: new mwemory size
+ * Return: pointer or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr != NULL)
+	{
+		memcpy(new_ptr, ptr, old_size);
+		free(ptr);
+	}
+
+	return (new_ptr);
+}
