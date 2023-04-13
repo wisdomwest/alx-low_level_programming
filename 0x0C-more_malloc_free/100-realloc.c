86207b1873931b613d0b83b4c1fd81b3c88967ae
wisdomwest <harmonykoome@gmail.com>
2023-04-13 05:54:08 -0700

    better work

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	char *new_ptr;
 
 	if (new_size == 0)
 	{
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	}
 
-	if (new_size >  old_size)
+	if (new_size ==  old_size)
 	{
 		return (ptr);
 	}
