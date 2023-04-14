44066dd9c4ffcaae0df4d539ba8a547156b20813
wisdomwest <harmonykoome@gmail.com>
2023-04-14 02:56:00 -0700

    last chance

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,36 +13,38 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	char *cp, *f;
+	unsigned int i;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
-	}
+		new_ptr = malloc(new_size);
 
+		if (new_ptr == NULL)
+			return (NULL);
+
+		return (new_ptr);
+	}
 	if (new_size ==  old_size)
 	{
 		return (ptr);
 	}
-
-	new_ptr = malloc(new_size);
-
-	if (new_ptr != NULL)
+	cp = ptr;
+	new_ptr = malloc(sizeof(*cp) * new_size);
+	if (new_ptr == NULL)
 	{
-		if (new_size > old_size)
-		{
-			memcpy(new_ptr, ptr, old_size);
-		}
-		else
-		{
-			memcpy(new_ptr, ptr, new_size);
-		}
 		free(ptr);
+		return (NULL);
 	}
+	f = new_ptr;
+	for (i = 0; i < old_size && i < new_size; i++)
+		f[i] = *cp++;
+
+	free(ptr);
 	return (new_ptr);
 }
