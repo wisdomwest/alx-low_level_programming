34b3ad668165a383fa5bcd4a58ad63bbf798251f
wisdomwest <harmonykoome@gmail.com>
2023-04-17 02:55:31 -0700

    task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int namelen = 0;
+	int ownerlen = 0;
+	int i;
+
+	while (name[namelen] != '\0')
+	{
+		namelen++;
+	}
+	while (owner[ownerlen] != '\0')
+	{
+		ownerlen++;
+	}
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	char *namecpy = malloc(namelen);
+	char *ownercpy = malloc(ownerlen);
+
+	if (namecpy == NULL || ownercpy == NULL)
+	{
+		free(new_dog);
+		free(namecpy);
+		free(ownercpy);
+		return (NULL);
+	}
+	for (i = 0; i < namelen; i++)
+	{
+		namecpy[i] = name[i];
+	}
+	for (i = 0; i < ownerlen; i++)
+	{
+		ownercpy[i] = owner[i];
+	}
+	new_dog->name = namecpy;
+	new_dog->age = age;
+	new_dog->owner = ownercpy;
+	return (new_dog);
+}
