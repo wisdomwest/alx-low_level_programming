28a348de9dd42d9b773c9ba76a8169bf1d69f6b7
wisdomwest <harmonykoome@gmail.com>
2023-04-17 02:28:48 -0700

    update for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,17 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 #endif
