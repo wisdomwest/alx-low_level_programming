0279ffd8fb7050e9a14f0d280237fdbbc97df59d
wisdomwest <harmonykoome@gmail.com>
2023-04-17 02:57:51 -0700

    error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,6 +6,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int namelen = 0;
 	int ownerlen = 0;
 	int i;
+	char *namecpy, *ownercpy;
+	dog_t *new_dog;
 
 	while (name[namelen] != '\0')
 	{
@@ -16,15 +18,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ownerlen++;
 	}
 
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	char *namecpy = malloc(namelen);
-	char *ownercpy = malloc(ownerlen);
+	namecpy = malloc(namelen);
+	ownercpy = malloc(ownerlen);
 
 	if (namecpy == NULL || ownercpy == NULL)
 	{
