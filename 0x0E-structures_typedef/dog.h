9f6726af26585949cae05094f968b726b8d1a88a
wisdomwest <harmonykoome@gmail.com>
2023-04-17 02:30:49 -0700

    no need to define struct again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,18 +17,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * struct dog - dog
- * @name: name
- * @age: age
- * @owner: owner
- */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 #endif
