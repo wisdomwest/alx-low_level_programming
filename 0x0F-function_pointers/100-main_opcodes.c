26a80d616f3e6a669eede91b4d1636896b692f8d
wisdomwest <harmonykoome@gmail.com>
2023-04-19 05:49:51 -0700

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_opcodes(char *start, int num_bytes);
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on invalid argument count, 2 on negative byte count
+ */
+int main(int argc, char **argv)
+{
+	int num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes((char *)main, num_bytes);
+
+	return (0);
+}
+
+/**
+ * print_opcodes - prints the opcodes of a function
+ * @start: starting address of function
+ * @num_bytes: number of bytes to print
+ */
+void print_opcodes(char *start, int num_bytes)
+{
+	int i;
+	
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx", start[i]);
+	}
+
+	printf("\n");
+}
