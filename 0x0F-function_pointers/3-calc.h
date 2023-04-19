781b18d1e650aad0ef99a16b6d3804f2d079bacb
wisdomwest <harmonykoome@gmail.com>
2023-04-19 05:33:47 -0700

    fixed erros

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
 /**
  * struct op - Struct op
  *
@@ -13,6 +15,11 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
