0be9631f504083e4aff5cff6fd7e0e5d549e6a6d
wisdomwest <harmonykoome@gmail.com>
2023-04-30 02:55:38 -0700

    task 4 easy

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees linked list
+ * @head: pointer to start of node
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(temp);
+	}
+}
