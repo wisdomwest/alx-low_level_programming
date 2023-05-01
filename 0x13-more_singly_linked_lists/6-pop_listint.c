191a7dc0718f80139f4784407ea333dc656a4a50
wisdomwest <harmonykoome@gmail.com>
2023-05-01 05:55:57 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * pop_listint -delete head node
+ * @head: head on linked list
+ * return: 0 or data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int data;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+
+	tmp = *head;
+	data = tmp->n;
+	*head = tmp->next;
+	free(tmp);
+
+	return(data);
+}
