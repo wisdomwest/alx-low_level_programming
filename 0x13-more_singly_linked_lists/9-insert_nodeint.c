6e2c8db385e319d0469e7a3d5adb8d859b4c0941
wisdomwest <harmonykoome@gmail.com>
2023-05-03 06:51:16 -0700

    working on task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node at index
+ * @head: beginning of list
+ * @idx: index
+ * @n: data to be stored
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
