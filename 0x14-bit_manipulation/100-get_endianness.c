1844359bbabcf643bd68eb17b200562bee5db239
wisdomwest <harmonykoome@gmail.com>
2023-05-04 13:18:49 -0700

    task 6 reserch on endinness testing how it is stored

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - get endianness
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+{
+	unsigned short x = 45;
+	char *c = (char *) &x;
+
+	if ((int) *c == 0x2D)
+		return (1);
+	else
+		return (0);
+}
