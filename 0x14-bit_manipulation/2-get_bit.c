3f8ccd10ac1803b182bae214850ea02ba541b414
wisdomwest <harmonykoome@gmail.com>
2023-05-04 05:45:56 -0700

    task 3 getting the hang of bit masks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at index using shift
+ * @n: number to check
+ * @index: index to check at binary rep on n
+ * Return: 1 or 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit_mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	bit_mask = 1UL << index;
+
+	if ((n & bit_mask) == 0)
+		return (1);
+	else
+		return (0);
+}
