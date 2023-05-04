129816bb2d25d2a2069e44add16f57f3413ae7e5
wisdomwest <harmonykoome@gmail.com>
2023-05-04 07:13:12 -0700

    jfpweifhnw

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -30,8 +30,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor;
 
-	if ( n == 0 || m == 0)
-		return (0);
+	if (n == 0 || m == 0)
+		return (-1);
 
 	xor = n ^ m;
 
