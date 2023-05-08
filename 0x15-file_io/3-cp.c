32cff4b054167e5697ab96c28c3342f3a2a071ce
wisdomwest <harmonykoome@gmail.com>
2023-05-08 13:22:57 -0700

    dude i hope this works

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error - print errro message
+ * @msg: message
+ * @filename: filename
+ * @code: exit code
+ */
+void error(char *msg, const char *filename, int code)
+{
+	dprintf(STDERR_FILENO, msg, filename);
+	exit(code);
+}
+
+/**
+ * main - entry point cp function
+ * @argc: counter
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	const char *from, *to;
+
+	int fd[2];
+	char buff[BUFFER_SIZE];
+	ssize_t r, w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	from = argv[1];
+	to = argv[2];
+
+	fd[0] = open(from, O_RDONLY);
+
+	if (fd[0] < 0)
+	{
+		error("ERROR: Can't read from file %s\n", from, 98);
+	}
+
+	fd[1] = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd[1] < 0)
+		error("ERROR: Can't write to %s", to, 99);
+
+	r = read(fd[0], buff, BUFFER_SIZE);
+	if (r < 0)
+		error("ERROR: Can't read from file %s\n", from, 98);
+
+	w = write(fd[1], buff, r);
+
+	if (w != r)
+		error("ERROR: Can't write to %s", to, 99);
+
+	if (close(fd[0]) < 0)
+		error("Error: Can't close fd %d", from, 100);
+
+	if (close(fd[1]) < 0)
+		error("Error: Can't close fd %d", to, 100);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
+#define BUFFER_SIZE 1024
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
