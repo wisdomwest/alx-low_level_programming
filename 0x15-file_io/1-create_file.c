2472ecddf4f1b9a870e4116f2e5841bf66d4dd90
wisdomwest <harmonykoome@gmail.com>
2023-05-08 08:12:30 -0700

    betty error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename , O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd < 0)
 		return (-1);
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		n = write(fd, text_content, strlen(text_content));
 
-		if ( n < 0)
+		if (n < 0)
 		{
 			close(fd);
 			return (-1);
