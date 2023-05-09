ee3a099eb13e3eaa69ff86922dc59a1d1cbb2fbc
wisdomwest <harmonykoome@gmail.com>
2023-05-09 03:04:10 -0700

    trying to make lines fewer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,36 +34,24 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "\n");
 		exit(97);
 	}
-
 	from = argv[1];
 	to = argv[2];
 
 	fd[0] = open(from, O_RDONLY);
-
 	if (fd[0] < 0)
-	{
 		error("ERROR: Can't read from file %s\n", from, 98);
-	}
-
 	fd[1] = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (fd[1] < 0)
 		error("ERROR: Can't write to %s", to, 99);
-
 	r = read(fd[0], buff, BUFFER_SIZE);
 	if (r < 0)
 		error("ERROR: Can't read from file %s\n", from, 98);
-
 	w = write(fd[1], buff, r);
-
 	if (w != r)
 		error("ERROR: Can't write to %s", to, 99);
-
 	if (close(fd[0]) < 0)
 		error("Error: Can't close fd %d", from, 100);
-
 	if (close(fd[1]) < 0)
 		error("Error: Can't close fd %d", to, 100);
-
 	return (0);
 }
