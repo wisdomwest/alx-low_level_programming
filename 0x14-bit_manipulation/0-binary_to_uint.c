361cf46f554258f20abb23fc653d56600637309a
wisdomwest <harmonykoome@gmail.com>
2023-07-06 02:29:52 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * binary_to_int - convert binary to base 10
+  * @b: pointer to binary
+  * Return: 0 or base 10
+  */
+
+unsigned int binary_to_int(const char *b)
+{
+	int decimal;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b == '0' || *b == '1')
+		{
+			decimal = decimal * 2 + (*b - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
+
+	return (decimal);
+}
