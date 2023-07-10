eb1a824bc71e1fe9af120e603918a546f046fc28
wisdomwest <harmonykoome@gmail.com>
2023-07-10 02:49:12 -0700

    task 0 and header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and print to POSIX
+ * @filename: pointer to file
+ * @letters: number of letters to print
+ * Return: 0 or letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buff;
+	ssize_t r_read, w_write;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(letters * sizeof(char));
+
+	if (buff == NULL)
+		return (0);
+
+	r_read = read(fd, buff, letters);
+
+	if (r_read == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	w_write = write(STDOUT_FILENO, buff, s_read);
+
+	if (w_writee == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
+	free(buff);
+	close(fd);
+	return (w_write);
+}
