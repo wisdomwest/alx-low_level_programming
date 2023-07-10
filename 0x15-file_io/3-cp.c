af743068740db49e1b5c9feb52ca50f37966512f
wisdomwest <harmonykoome@gmail.com>
2023-07-10 10:03:05 -0700

    chnaged everything

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,83 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * error97 - print error 97
+ * Return: void
+*/
+void error97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
 
 /**
- * error - print error
- * @msg: message
- * @filename: filename
- * @code: exit code
- */
-void error(char *msg, const char *filename, int code)
+ * error98 - print error 98
+ * @file: file
+ * Return: void
+*/
+void error98(char *file)
 {
-	dprintf(STDERR_FILENO, msg, filename);
-	exit(code);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+/**
+ * error99 - print error 99
+ * @file: file
+ * Return: void
+*/
+void error99(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+/**
+ * error100 - print error 100
+ * @fd: file descriptor
+ * Return: void
+*/
+void error100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
 
 /**
  * main - entry point
- * @argc: counter
- * @argv: arguments
- * Return: 0
- */
-
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * Return: 0 on success, -1 on failure
+*/
 int main(int argc, char *argv[])
 {
-	const char *from, *to;
-
-	int fd[2];
-	char buff[BUFFER_SIZE];
-	ssize_t r, w;
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[BUFFER_SIZE];
+	ssize_t r;
+	ssize_t w;
+	int fd_from, fd_to;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to", argv[0]);
-		dprintf(STDERR_FILENO, "\n");
-		exit(97);
-	}
-	from = argv[1];
-	to = argv[2];
+		error97();
 
-	fd[0] = open(from, O_RDONLY);
-	if (fd[0] < 0)
-		error("ERROR: Can't read from file %s\n", from, 98);
-	fd[1] = open(to, O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (fd[1] < 0)
-		error("ERROR: Can't write to %s", to, 99);
-	r = read(fd[0], buff, BUFFER_SIZE);
-	while (r > 0)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error98(argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error99(argv[2]);
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		w = write(fd[1], buff, r);
-		if (w == -1)
-			error("ERROR: Can't write to %s", to, 99);
+		w = write(fd_to, buffer, r);
 		if (w != r)
-			error("ERROR: Can't write to %s", to, 99);
+			error99(argv[2]);
 	}
 	if (r == -1)
-		error("ERROR: Can't read from file %s\n", from, 98);
-	if (close(fd[0]) < 0)
-		error("Error: Can't close fd %d", from, 100);
-	if (close(fd[1]) < 0)
-		error("Error: Can't close fd %d", to, 100);
+		error98(argv[1]);
+	if (close(fd_from) == -1)
+		error100(fd_from);
+	if (close(fd_to) == -1)
+		error100(fd_to);
+
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
