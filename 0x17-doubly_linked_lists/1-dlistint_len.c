cf5b128afc68bb9d0408a871dfed275d75fe4341
wisdomwest <harmonykoome@gmail.com>
2023-08-17 13:16:23 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+
+/**
+  * dlistint_len - number of elements
+  * @h: pointer
+  * Return: count
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
