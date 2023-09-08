c0722fdd1f06becc35117b595c908a4bcbc1e2a3
wisdomwest <harmonykoome@gmail.com>
2023-09-08 04:40:00 -0700

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieve the value of key
+ * @ht: A pointer to the hash table.
+ * @key: The key 
+ * Return: NULL or key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *x;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	x = ht->array[index];
+	while (x && strcmp(node->key, key) != 0)
+		x = x->next;
+
+	return ((x == NULL) ? NULL : x->value);
+}
