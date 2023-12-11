30f760245ae954fa80d12c8f8b90be859d3e7478
wisdomwest <harmonykoome@gmail.com>
2023-12-11 17:19:27 +0300

    done with mandatory tasks including a binary search alg

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+  * binary_search - binary search algorithm
+  * @array: pointer to first element in array
+  * @size: number of elements in array
+  * @value: value to search for
+  * Return: index where value is located
+  */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	size_t L = 0;
+	size_t R = size -1;
+
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+
+	while (L <= R)
+	{
+		printf("Searching in array: ");
+		for (i = L; i < R; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n, ", array[i]);
+
+		i = L + (R - L) / 2;
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		if (array[i] < value)
+		{
+			L = i + 1;
+		}
+		else
+		{
+			R = i - 1;
+		}
+	}
+	return (-1);
+}
